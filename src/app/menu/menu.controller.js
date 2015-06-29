8376b6d799546785d4f8497d28cfed5892bff483
jiwang <jiwang@alibaba-inc.com>
2015-06-29 23:09:44 +0800

    route to order detail work

--- src/app/menu/menu.controller.js
+++ src/app/menu/menu.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('dian')
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/menu/orders/:id', {
+        template: '<div>123</div>'
+      })
+  })
   .controller('MenuCtrl', function ($scope) {
 
   });
