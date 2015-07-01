3687b2cb7abdce421e56b86d864bbd48ef6d7f18
jiwang <jiwang@alibaba-inc.com>
2015-07-01 21:47:50 +0800

    e2e test work

--- e2e/menu.orders.spec.js
+++ e2e/menu.orders.spec.js
@@ -1,11 +1,29 @@
 'use strict';
 
 describe('mene orders view', function() {
-  beforeEach(function() {
+  var menu_orders_1_url;
 
+  browser.addMockModule('httpmock', function() {
+    angular.module('httpmock', ['ngMockE2E'])
+      .run(function($httpBackend) {
+        $httpBackend.whenGET(/\.html$/).passThrough();
+
+        $httpBackend.expectGET(menu_orders_1_url = '/#/menu/orders/1')
+          .respond({
+            name: 'my_restuarant'
+          });
+
+      });
+  });
+
+  beforeEach(function() {
+    //browser.get(menu_orders_1_url = '/#/menu/orders/1');
+    browser.get(menu_orders_1_url = 'http://localhost:3000/#/menu/orders/1');
   });
 
   it('should display order detail', function() {
-    expect(1).toBe(2);
-  })
+    browser.debugger();
+    expect(element(by.css('.restuarant-name')).getText()).toBe('my_restuarant');
+  });
 });
+
