637e5e6477f0d3184b96b1ca4e66d9291f6f0579
jiwang <jiwang@alibaba-inc.com>
2015-07-04 19:19:35 +0800

    html of restaurant catogries work

--- e2e/menu.orders.spec.js
+++ e2e/menu.orders.spec.js
@@ -3,18 +3,13 @@
 describe('mene orders view', function() {
   var menu_orders_1_url, host;
   host = require('./config.e2e.test').localhost;
-  //browser.addMockModule('httpmock', require('./httpmock'));
 
   beforeEach(function() {
     browser.get(menu_orders_1_url = (host + '/#/menu'));
   });
 
   it('should display orders total number', function() {
-    expect(element(by.model('orders.length'))).toBe(1);
-  });
-
-  afterEach(function() {
-    //browser.clearMockModules();
+    expect(element(by.binding('orders.length')).getText()).toBe('2');
   });
 });
 
