469fdd222dd16e8c7f1759040637f23650fd8184
jiwang <jiwang@alibaba-inc.com>
2015-07-07 22:34:40 +0800

    add unit option to product

--- src/app/directives/product.js
+++ src/app/directives/product.js
@@ -4,7 +4,8 @@ angular.module('dian')
 .directive('product', function() {
   return {
     scope: {
-      item: '='
+      item: '=',
+      unit: '='
     },
     templateUrl: 'app/directives/product.html'
   };
