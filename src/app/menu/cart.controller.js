39bc2331fb99a770b54fbcfc34362c7e0159bf72
jiwang <jiwang@alibaba-inc.com>
2015-07-07 21:55:33 +0800

    wifi info

--- src/app/menu/cart.controller.js
+++ src/app/menu/cart.controller.js
@@ -21,21 +21,35 @@ angular.module('dian')
   };
 
   fetch('restaurant-cart')({table_id: 2}, {
-      openid: restaurant_openid = 'can123',
-      wp_openid: member_openid = 123
-    }).then(function(res) {
-      console.log('cart');
-      console.log(res.data);
-      $scope.cart = res.data;
-      cart_id = $scope.cart.id;
-      $scope.products = utils.map($scope.cart.cart_items || [], function(v) {
-        //extend product into cart_item for ng-repeat
-        delete v.product.id;
-        return angular.extend(v, v.product);
-      });
-      $scope.total_price = utils.listItemSum($scope.products, 'price');
-      $scope.total_count = utils.listItemSum($scope.cart.cart_items, 'count');
+    openid: restaurant_openid = 'can123',
+    wp_openid: member_openid = 123
+  }).then(function(res) {
+    console.log('cart');
+    console.log(res.data);
+    $scope.cart = res.data;
+    cart_id = $scope.cart.id;
+    $scope.products = utils.map($scope.cart.cart_items || [], function(v) {
+      //extend product into cart_item for ng-repeat
+      delete v.product.id;
+      return angular.extend(v, v.product);
     });
+    $scope.total_price = utils.listItemSum($scope.products, 'price');
+    $scope.total_count = utils.listItemSum($scope.cart.cart_items, 'count');
+  });
+
+  fetch('restaurant')(null, {
+    openid: restaurant_openid
+  }).then(function(res) {
+    console.log('restaurant');
+    console.log(res.data);
+    $scope.restaurant = res.data;
+    /* for debug
+    $scope.restaurant = angular.extend($scope.restaurant, {
+      wifi_name: 123,
+      wifi_password: 213
+    })
+    */
+  });
 }]);
 
 
