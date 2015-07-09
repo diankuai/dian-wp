0c4e908a52a0b6fc72a3c7ebd9f03e81f4eadf0b
jiwang <jiwang@alibaba-inc.com>
2015-07-09 23:23:56 +0800

    member id interceptor

--- src/app/index.js
+++ src/app/index.js
@@ -27,4 +27,35 @@ angular.module('dian', ['ngCookies', 'ngTouch', 'ngRoute', 'dianApp'])
         redirectTo: '/'
       });
   })
+  /*
+  .factory('memberIdInterceptor', ['$q', '$cookies', '$location', '$injector',
+           function ($q, $cookies, $location, $injector) {
+      return {
+          request: function (config) {
+              var weixin, headerOk;
+              config.headers = config.headers || {};
+              headerOk = $q.defer()
+              weixin = $injector.get('weixin');
+              weixin.getMember().then(function(res) {
+                config.headers['X-Member-Id'] = res && res.data && res.data['wp_openid'];
+                headerOk.resolve(config);
+              }, function(res) {
+                headerOk.reject(res)
+              });
+              return headerOk.promise;
+          }
+      };
+  }])
+  */
+
+  /*
+  .config(['$httpProvider', function($httpProvider) {
+    //$httpProvider.interceptors.push('memberIdInterceptor');
+    console.log(GLOBAL);
+  }])
+
+  .run(function() {
+    GLOBAL = 123;
+  })
+  */
 ;
