7241a9aedd05169d8e8ab2096fa3d541401ae4de
jiwang <jiwang@alibaba-inc.com>
2015-07-14 21:17:20 +0800

    queue item router work"

--- src/app/queue/queue.controller.js
+++ src/app/queue/queue.controller.js
@@ -1,6 +1,16 @@
 'use strict';
 
 angular.module('dian')
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/queue_items/:id', {
+        templateUrl: 'app/queue/queue_items.html',
+        controller: 'QueueItemsCtrl'
+      });
+  })
+  .controller('QueueItemsCtrl', function() {
+
+  })
   .controller('QueueCtrl', function (config, $scope, $http) {
     $http.get(config.api_url + '/wp/registration/list-current-registration/').then(function(res) {
       console.log('queue list');
