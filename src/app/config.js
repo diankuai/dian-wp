418adfe7dc901688940ba45bc59a2e3669d9368e
jiwang <jiwang@alibaba-inc.com>
2015-07-14 20:50:14 +0800

    change api server url

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 angular.module('dian')
   .constant('config', {
-    api_url: 'http://dev.dk26.com:8080',
+    api_url: 'http://api.dk26.com:8080',
     cdn_file_url: 'http://7u2ghq.com1.z0.glb.clouddn.com/',
   });
 
