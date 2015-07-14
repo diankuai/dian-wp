fb94928a2e79622b59f1af2d2ed0434400c9259a
jiwang <jiwang@alibaba-inc.com>
2015-07-14 21:12:01 +0800

    queue portal's list ok

--- src/app/queue/queue.controller.js
+++ src/app/queue/queue.controller.js
@@ -1,6 +1,10 @@
 'use strict';
 
 angular.module('dian')
-  .controller('QueueCtrl', function ($scope) {
-
+  .controller('QueueCtrl', function (config, $scope, $http) {
+    $http.get(config.api_url + '/wp/registration/list-current-registration/').then(function(res) {
+      console.log('queue list');
+      console.log(res.data);
+      $scope.queue = res.data;
+    });
   });
