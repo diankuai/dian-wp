88bfa95d00c395bc9fc78787ea0ff949a63f8411
jiwang <jiwang@alibaba-inc.com>
2015-07-14 21:34:43 +0800

    queue item work

--- src/app/queue/queue.controller.js
+++ src/app/queue/queue.controller.js
@@ -8,8 +8,20 @@ angular.module('dian')
         controller: 'QueueItemsCtrl'
       });
   })
-  .controller('QueueItemsCtrl', function() {
-
+  .controller('QueueItemsCtrl', function(config, $scope, $http, $routeParams) {
+    var queue_item_id;
+    console.log('queue item id');
+    console.log($routeParams.id);
+    queue_item_id = $routeParams.id;
+    $http.get(config.api_url + '/wp/registration/get-detail-registration/', {
+      params: {
+        id: queue_item_id
+      }
+    }).then(function(res) {
+      console.log('queue_item');
+      console.log(res.data);
+      $scope.queue_item = res.data;
+    });
   })
   .controller('QueueCtrl', function (config, $scope, $http) {
     $http.get(config.api_url + '/wp/registration/list-current-registration/').then(function(res) {
