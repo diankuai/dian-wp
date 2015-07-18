fbf791b6191e5e0db43af14b8f26008880492e84
jiwang <jiwang@alibaba-inc.com>
2015-07-18 19:37:04 +0800

    debug when server down

--- src/app/config.js
+++ src/app/config.js
@@ -2,5 +2,6 @@ angular.module('dian')
   .constant('config', {
     api_url: 'http://api.dk26.com:8080',
     cdn_file_url: 'http://7u2ghq.com1.z0.glb.clouddn.com/',
+    debug: true
   });
 
--- src/app/queue/queue.controller.js
+++ src/app/queue/queue.controller.js
@@ -35,9 +35,17 @@ angular.module('dian')
     });
   })
   .controller('QueueCtrl', function (config, $scope, $http) {
-    $http.get(config.api_url + '/wp/registration/list-current-registration/').then(function(res) {
+    $http.get(config.api_url + '/wp/registration/list-current-registration/')
+    .then(function(res) {
       console.log('queue list');
       console.log(res.data);
-      $scope.queue = res.data;
+      $scope.queue = res.data || [];
+      
     });
+    $scope.queue = [{
+        restaurant: {
+          name: 'this is restaurant'
+        },
+        id: 1
+      }];
   });
