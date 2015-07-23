99dffb739bf8ee5cf73292971f1f0e59cc1c8817
jiwang <jiwang@alibaba-inc.com>
2015-07-23 23:30:04 +0800

    next photo work

--- src/app/photo/photo.controller.js
+++ src/app/photo/photo.controller.js
@@ -11,17 +11,30 @@ angular.module('dian')
     */
   })
   .controller('PhotoCtrl', function (config, $http, $scope) {
+    var photos_limit = 5;
 
-    $http.get(config.api_url + '/wp/photo/get-next-photo-list/', {
+    getPhotos();//先获取几张照片
+    $scope.photos = [{alt: 123}, {alt: 456}];//for debug
+    $scope.index = 0;
+
+    $scope.next_photo = function() {
+      $scope.index++;
+      $scope.index %= photos_limit;//始终使index < limt
+      console.log('cur photo index');
+      console.log($scope.index);
+    };
+
+    function getPhotos() {
+      return $http.get(config.api_url + '/wp/photo/get-next-photo-list/', {
       /* default is 5, which is configed by server
       params: {
         limit: 5
       }
       */
-    }).then(function(res) {
-      console.log('5 photos');
-      console.log(res.data);
-    });
-    $scope.photos = [{alt: 123}, {alt: 456}];
-    $scope.index = 0;
+      }).then(function(res) {
+        console.log('5 photos');
+        console.log(res.data);
+        return res;
+      });
+    }
   });
