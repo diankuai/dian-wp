c463964f801e11c83a9b65a53e9719b91a150e77
david <deggs.k@gmail.com>
2015-09-14 13:18:45 +0800

    test weixin scanQRCode

--- src/app/queue/queue.controller.js
+++ src/app/queue/queue.controller.js
@@ -2,12 +2,24 @@
 
 angular.module('dian')
 
-  .controller('QueueCtrl', function (config, $scope, $http, utils) {
+  .controller('QueueCtrl', function (config, $scope, $http, utils, weixin) {
     utils.setTitle('排队');
     $http.get(config.apiUrl + '/wp/registration/list-current-registration/')
     .then(function(res) {
       $scope.queue = res.data || [];
     });
+
+    $scope.scan = function () {
+      weixin.safeExec('scanQRCode', {
+        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+        scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+        success: function (res) {
+          // 当needResult 为 1 时，扫码返回的结果
+          console.log(res);
+        }
+      });
+    };
+
   })
 
   .controller('QueueDetailCtrl', function(config, $scope, $http, $routeParams, utils) {
