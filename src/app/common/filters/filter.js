e5925b8697d3a04f9edae31ea99e5fa3662c4f11
david <deggs.k@gmail.com>
2015-09-15 23:29:56 +0800

    improve style of queue module

--- src/app/common/filters/filter.js
+++ src/app/common/filters/filter.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('dian')
+
+  .filter('datetime', [
+    function () {
+      return function (datetime) {
+        //2015-03-21T00:00:00Z
+        if (datetime) {
+          var re = datetime.match(/\d{4}-(\d{2})-(\d{2})T(\d{2}:\d{2}):\d{2}Z/);
+          return (re.length > 3) ? (re[1] + '月' + re[2] + '日 ' + re[3]) : datetime;
+        } else {
+          return datetime;
+        }
+      };
+    }
+  ])
+
   .filter('order_time', [
     function() {
       return function(order_time_utc) { //2015-03-21T00:00:00Z
