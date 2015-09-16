59e73d94ee752b0e7870ed4bf5c0a2f6b055e6d6
david <deggs.k@gmail.com>
2015-09-16 18:35:22 +0800

    format datetime without TZ

--- src/app/common/filters/filter.js
+++ src/app/common/filters/filter.js
@@ -7,7 +7,7 @@ angular.module('dian')
       return function (datetime) {
         //2015-03-21T00:00:00Z
         if (datetime) {
-          var re = datetime.match(/\d{4}-(\d{2})-(\d{2})T(\d{2}:\d{2}):\d{2}Z/);
+          var re = datetime.match(/\d{4}-(\d{2})-(\d{2})T(\d{2}:\d{2}):\d{2}/);
           return (re.length > 3) ? (re[1] + '月' + re[2] + '日 ' + re[3]) : datetime;
         } else {
           return datetime;
