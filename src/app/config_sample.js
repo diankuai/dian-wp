5bcb8c08e0cf6546f5a67c4ecb29dc2baf4383e6
david <deggs.k@gmail.com>
2015-11-03 19:04:27 +0800

    add config item

--- src/app/config_sample.js
+++ src/app/config_sample.js
@@ -2,7 +2,11 @@
 angular.module('dian')
   .constant('config', {
     apiUrl: 'http://api.dk26.com',
+
+    // qiniu image
     cdnFileUrl: 'http://7u2ghq.com1.z0.glb.clouddn.com/',
+    productIconFormat: '-square100',
+
     debug: true
   });
 */
