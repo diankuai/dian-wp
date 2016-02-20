6b2c43fd40760870889acff8a178f81c1059c363
david <deggs.k@gmail.com>
2016-02-20 20:03:42 +0800

    fix of get cookie

--- src/app/common/services/member.js
+++ src/app/common/services/member.js
@@ -6,7 +6,7 @@ angular.module('dian')
 /* 获取微信用户信息 */
 .service('memberInfo', ['config', '$http', '$cookies',
     function(config, $http, $cookies) {
-  var openid = $cookies.member_id;
+  var openid = $cookies.get('member_id');
   var promise = $http.get(config.apiUrl + '/wp/account/get-member-by-openid/', {
       params: {
         openid: openid 
